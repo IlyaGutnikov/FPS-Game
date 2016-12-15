91adf2a12a724d6cf2d48904a799be0442ea4925
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-12-15 14:47:07 +0300

    Finished E01.

---
+++ FPS_Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 5f;
+
+    private PlayerMotor motor;
+
+    void Start()
+    {
+
+        motor = GetComponent<PlayerMotor>();
+
+    }
+
+    void Update()
+    {
+        // Get movment
+        float _xMovment = Input.GetAxisRaw("Horizontal");
+        float _zMovment = Input.GetAxisRaw("Vertical");
+
+        Vector3 movHorizontal = transform.right * _xMovment;
+        Vector3 movVertical = transform.forward * _zMovment;
+
+        //Нормализация говорит что длина векторов не важна => длина вектора всегда 1
+        Vector3 _velocity = (movHorizontal + movVertical).normalized * speed;
+
+        motor.Move(_velocity);
+    }
+}
