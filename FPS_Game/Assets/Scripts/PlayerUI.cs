beac04a4ed310c49016ebeeb66678f42e3befd52
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2017-01-18 22:03:06 +0300

    Start 14 lesson

---
+++ FPS_Game/Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerUI : MonoBehaviour {
+
+   [SerializeField]
+    RectTransform fuelFill;
+
+    void SetFuelAmount(float _amount) {
+
+        fuelFill.localScale = new Vector3(1f, _amount, 1f);
+    }
+}
